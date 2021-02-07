bfb353a0553057e3430171ec3104907d06774c8f
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-02-07 14:09:33 +0000

    Accepting post and get request from login and signp route

--- note/auth.py
+++ note/auth.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def signup():
     return render_template('register.html')
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
