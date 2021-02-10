0358504cc4f53a1135da8c541f2267c0a1b6e2ae
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-02-10 11:32:05 +0000

    Created user dasboard and route, created login manager functions and user route authorization

--- note/auth.py
+++ note/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from . import db
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
@@ -14,7 +15,11 @@ def signup():
         password = request.form.get('password1')
         confirm = request.form.get('password2')
 
-        if len(full_name) < 4:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Sorry, this email is already a user')
+        elif len(full_name) < 4:
             flash('Full Name must be greater than 4 characters', 'danger')
         elif len(email) < 4:
             flash('Email must be greater than 4 characters', 'danger')
@@ -32,8 +37,25 @@ def signup():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully', 'success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password', 'danger')
+        else:
+            flash('Email does not exist', 'danger')
+
     return render_template('login.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h1> Logout Now </h1>"
+    logout_user()
+    return redirect(url_for('views.index'))
