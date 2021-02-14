84530ec900f795874ccf14f70c4994a98fc781b9
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-02-14 11:45:09 +0000

    Created add category features

--- note/views.py
+++ note/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
+from .models import Category
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -7,7 +9,43 @@ views = Blueprint("views", __name__)
 def index():
     return render_template("index.html", user=current_user)
 
-@views.route('/home')
+@views.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template('dashboard.html')
+    if request.method == 'POST':
+        title = request.form.get('title')
+        if len(title) < 1:
+            flash("Sorry, Titlecan't be empty", "danger") 
+            return redirect(url_for('views.home'))
+        else:
+            new_category = Category(title=title)
+            db.session.add(new_category)
+            db.session.commit()
+
+            flash("Category created", "danger") 
+            return redirect(url_for('views.home'))
+
+    user_id = current_user.get_id()
+    categories = Category.query.filter_by(id=user_id).all()
+    if categories:
+        return render_template("dashboard.html", user=current_user, categories=categories)
+    else:
+        flash("You have not created a category yet", "success")
+        return render_template("dashboard.html", user=current_user)
+
+# @views.route('/category', methods=['POST'])
+# def add_category():
+#     pass
+
+@views.route('/categories/add_note', methods=['POST'])
+def add_note():
+    title = request.form.get('title')
+    body = request.form.get('body')
+
+    if len(body) < 1 or len(title) < 1:
+        flash("Sorry, Title and Body can't be empty", "danger") 
+        return redirect(url_for('home'))
+    else:
+        new_note = Note()
+        flash("Note has been added", "success")
+        return redirect(url_for('home')) 
